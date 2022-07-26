fec2076add0656ea6f6f862d630c96c69a6bea15
Conor White <conorwhite667@gmail.com>
2022-07-26 15:59:44 +0100

    finished the modelFromResultSet method in OrderDAO

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -1,5 +1,62 @@
 package com.qa.ims.persistence.dao;
 
-public class OrderDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Order;
+
+public class OrderDAO implements Dao<Order> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	@Override
+	public Order modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long id = resultSet.getLong("id");
+		Long itemId = resultSet.getLong("Item_Id");
+		Long customerId = resultSet.getLong("Customer_Id");
+		Long quantity = resultSet.getLong("Quantity");
+		Long totalCost = resultSet.getLong("Total_Cost");
+		return new Order(id, itemId, customerId, quantity, totalCost);
+	}
+
+	@Override
+	public List<Order> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order create(Order t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order update(Order t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Order readLatest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
