180696a653c1bbc55366773931236af58e753f80
Conor White <conorwhite667@gmail.com>
2022-07-28 15:45:29 +0100

    testUpdate() passes tests

--- src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
@@ -22,26 +22,6 @@ public class OrderDAOTest {
 		DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
 	} 
 	@Test
-	public void testCreate() {
-		final Order created = new Order(1L);
-		assertEquals(created, DAO.create(created));
-	}
-	@Test
-	public void testReadAll() {
-		List<Order> expected = new ArrayList<>();
-		expected.add(new Order(1L));
-		assertEquals(expected, DAO.readAll()); 
-	}
-	@Test
-	public void testReadLatest() {
-		assertEquals(new Order(1L), DAO.readLatest());
-	}
-	@Test
-	public void testRead() {
-		final long ID = 1L;
-		assertEquals(new Order(ID), DAO.read(ID));
-	}
-	@Test
 	public void testUpdate() {
 		final Order updated = new Order(1L, 1L, 1L);
 		assertEquals(updated, DAO.update(updated));
--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,3 +1,4 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `item` (`itemName`, `price`, `stock`) VALUES ('spoon', 3, 3);
-INSERT INTO `order` (OrderId, customerId) VALUES (1L, 1);
+INSERT INTO `order` (customerId) VALUES (1);
+INSERT INTO `OrderedProducts` (fk_OrderId, fk_itemId, Quantity) values (1L,1L,199);
